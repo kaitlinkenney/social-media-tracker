5cd9422cc73be73a7fc99c1a4b2f6c5041def6b4
kaitlinkenney <kaitlin.kenney96@gmail.com>
2021-05-25 12:36:33 -0400

    linking to social media sites

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Script from './pages/script'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Script/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/script.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+
+
+function Home(){
+
+return (
+    <div>
+        <h1> hello</h1>
+        {/* <a href="http://www.facebook.com">
+        <button href="http://www.facebook.com" target="_blank"> facebook </button>
+        </a> */}
+        <a href="http://www.instagram.com">
+        <button href="http://www.instagram.com" target="_blank"> Instagram </button>
+        </a>
+    </div>
+)
+
+}
+
+
+export default Home;
