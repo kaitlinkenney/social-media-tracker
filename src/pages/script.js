acc3bb3e753ec057eecc4ca0b799cee0d267cf71
kaitlinkenney <kaitlin.kenney96@gmail.com>
2021-07-05 13:09:47 -0400

    counter going down to zero

--- src/pages/script.js
+++ src/pages/script.js
@@ -1,25 +1,92 @@
 import React, { useState, useEffect } from "react";
 
 
-function Home(){
+function Home() {
 
 
-    function openInsta(){
-            window.open("http://www.instagram.com")
+    function openInsta() {
+        let insta = window.open("http://www.instagram.com")
+        // myFunction()
+        // updateTime()
+        // console.log(timer)
+        // if (timer == 0) {
+        //     insta.close()
+        // }
     }
-return (
-    <div>
-        <h1> hello</h1>
-      <button
-      onClick={openInsta}>Instagram</button>
-        {/* <a href="http://www.instagram.com">
+
+    //console.log("30")
+    let timer = 105;
+
+    // function updateTime(){
+    //     while (timer > 0) {
+    //         timer --;
+    //     }
+    //     console.log(timer)
+    // }
+
+    function updateTime() {
+        while (timer > 0){
+        timer--
+     }
+        
+      }
+      updateTime()
+
+    //   function runFunction(callback, interval, repeat){
+    //       let repeated = 0;
+    //       const intervalTask = setInterval(doTask, interval)
+    //     function doTask(){
+    //         if (repeated < repeat){
+    //             callback()
+    //             repeated +=1
+    //         } else {
+    //            clearInterval(intervalTask)
+    //         }
+    //     }
+    //   }
+
+    //   runFunction(updateTime, 3000, 5)
+
+      //var min;
+    //   window.onload = function() {
+    //     setInterval(function() {
+           
+    //         min = 60;
+    //     min--;
+        
+    //       console.log(min)
+          
+    //     }, 1000);
+    
+    //   }
+
+    //   function secondsLeft() {
+    //     let now = new Date().getSeconds()
+    //   }
+    //   function init() {
+    //     timer = setInterval(secondsLeft(), 1000);
+    //   }
+   
+    //   init()
+        // setInterval(updateTime, 3000)
+    
+    
+   
+
+    return (
+        <div>
+            <h1> hello</h1>
+            <p>Time: {timer}</p>
+            <button
+                onClick={openInsta}>Instagram</button>
+            {/* <a href="http://www.instagram.com">
         <button href="http://www.instagram.com" > Instagram </button>
         </a> */}
 
-   {/* <a href="https://www.instagram.com" target="_blank"><button>Instagram</button></a> */}
+            {/* <a href="https://www.instagram.com" target="_blank"><button>Instagram</button></a> */}
 
-    </div>
-)
+        </div>
+    )
 
 }
 
